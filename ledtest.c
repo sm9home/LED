ca8c02da1fc655f1b8b84b7924820f5280f8f83f
Lim, Seongmin <sm9home@naver.com>
2020-11-11 23:03:06 +0900

    ledtest
    test

--- ledtest.c
+++ ledtest.c
@@ -12,6 +12,15 @@ int main (void)
 	sleep(1);
 	ledLibRaw(0x0);
 	int i=0;
+	for (i=0; i<8; i++)
+	{
+		ledLibOnOff(i, 1);
+		sleep(1);
+		ledLibOnOff(1, 0);
+		sleep(1);
+	}
 	sleep(2);
 	ledLibExit();
+	
+	return 0;
 }
